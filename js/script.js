eb42a47bf079ae9f4c06b611327574d9a5d6e0ff
Kunkuma Geeth Prasanna <kunkumagp@gmail.com>
2021-08-22 22:28:23 +0530

    .

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var experiences = [
+    {
+        title:"Software Engineer",
+        company:"Sanmark Solutions pvt ltd.",
+        location:"Ranawiru Mw., Kahatapitiya, Batapola, Galle, Sri Lanka. 80320",
+        start:"",
+        end:""
+    },
+    {
+        title:"Web Admin",
+        company:"Fashion Bug",
+        location:"Pepiliyana, Sri Lanka.",
+        start:"",
+        end:""
+    },
+    {
+        title:"Executive Web Designer & Developer",
+        company:"Viyana Computer Company",
+        location:"Weliweriya - Gampaha, Sri Lanka.",
+        start:"",
+        end:""
+    },
+    {
+        title:"Web Engineer",
+        company:"Teata Group pvt Ltd",
+        location:"Kesbewa, Sri Lanka.",
+        start:"",
+        end:""
+    },
+    {
+        title:"Website Designer",
+        company:"VisitAir Information Centre (PVT) Ltd",
+        location:"Elpitiya, Sri Lanka.",
+        start:"",
+        end:""
+    }
+];
